662b85e7fce16ebd20cd2340476162893c4c715f
RanjeetDhole <ranjeetdhole26@gmail.com>
2025-08-17 00:36:09 +0530

    changes made

--- terraform-modules/vpc/variables.tf
+++ terraform-modules/vpc/variables.tf
@@ -25,7 +25,7 @@ variable "vpc_cidr" {
 variable "public_subnets" {
   description = "List of public subnets CIDRs"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24","10.0.4.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "tags" {
