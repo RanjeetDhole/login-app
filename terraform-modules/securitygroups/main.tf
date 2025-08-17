087bd9b6b9e72e02d850f5338a53a499a9f28dde
RanjeetDhole <ranjeetdhole26@gmail.com>
2025-08-17 11:18:48 +0530

    securitygroup

---
+++ terraform-modules/securitygroups/main.tf
@@ -0,0 +1,33 @@
+module "sg_web" {
+  source = "./terraform-modules/security-group"
+
+  name        = "${local.name}-web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow SSH"
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow HTTP"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow HTTPS"
+    }
+  ]
+
+  tags = local.tags
+}
